58d5f96842235f4a262630c74ad431f1910593a3
Philipp Schafft <philipp.schafft@nacamar.de>
2021-02-27 11:41:46 +0000

    Feature: Added default App code

---
+++ app/src/main/java/io/ybrid/app/example/android/ng/MainActivity.java
@@ -0,0 +1,13 @@
+package io.ybrid.app.example.android.ng;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
