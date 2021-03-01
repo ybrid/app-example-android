fec1f8ef1e8ff5947a1afb889891ef535d0c4c54
Philipp Schafft <philipp.schafft@nacamar.de>
2021-03-01 12:41:22 +0000

    Feature: Use Player from MainActivity

--- app/src/main/java/io/ybrid/app/example/android/ng/MainActivity.java
+++ app/src/main/java/io/ybrid/app/example/android/ng/MainActivity.java
@@ -2,12 +2,31 @@ package io.ybrid.app.example.android.ng;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import io.ybrid.app.example.android.ng.player.AndroidPlayer;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    @NotNull AndroidPlayer player = new AndroidPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_play).setOnClickListener(v -> player.play());
+        findViewById(R.id.button_swap).setOnClickListener(v -> player.swap());
+        findViewById(R.id.button_stop).setOnClickListener(v -> player.stop());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            player.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
