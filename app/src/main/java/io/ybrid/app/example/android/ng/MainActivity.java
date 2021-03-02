73ec4e8b666ef878d4e2755667b4b1bb2b60f934
Philipp Schafft <philipp.schafft@nacamar.de>
2021-03-02 16:42:09 +0000

    Fix: Corrected modifiers for AndroidPlayer instance

--- app/src/main/java/io/ybrid/app/example/android/ng/MainActivity.java
+++ app/src/main/java/io/ybrid/app/example/android/ng/MainActivity.java
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    @NotNull AndroidPlayer player = new AndroidPlayer();
+    private final @NotNull AndroidPlayer player = new AndroidPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
